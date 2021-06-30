57a932e474839a76c66a572722183fd84514585a
OctupusPrime <rasiapypkin2014@gmail.com>
2021-06-30 15:28:06 +0300

    alfa

--- src/components/TypeTest.vue
+++ src/components/TypeTest.vue
@@ -1,17 +1,19 @@
 <template>
     <article class="type-test-container">
-        <h1 class="type-test-title">Type test</h1>
+        <h1 class="type-test-title">|</h1>
         <div class="type-test-body">
-            <span class="body-letter" v-for="(letter, index) in arrayOfText" :key="index" 
-                                        :data-index="index"
-                                        :class="{'green': (index === rightLetter && !isRed), 'red': (index === rightLetter && isRed)}">
-                {{letter}}                 
-            </span>
+            <div class="type-test-body-letter-row" :style="translateX">
+                <span class="body-letter" v-for="(letter, index) in arrayOfText" :key="index" 
+                                            :class="{'green': (index === rightLetter && !isRed), 'red': (index === rightLetter && isRed)}">
+                    {{letter}}                 
+                </span>
+            </div>
         </div>
     </article>    
 </template>
 
 <script>
+let rowLetters = null;
 export default {
     name: 'TypeTest',
     props: {
@@ -22,49 +24,89 @@ export default {
     },
     data: () => {
         return {
+            isCaps: false,
             rightLetter: 0,
-            isRed: false
+            isRed: false,
+            offsetX: 0,
+
+            timerVal: 60
         }
     },
     computed: {
         arrayOfText() {
             return this.testText.split('')
+        },
+        translateX() {
+            return `transform: translateX(${-this.offsetX}px);`
+        }
+    },
+    watch: {
+        'rightLetter' () {
+            this.offsetX += rowLetters[this.rightLetter-1].getBoundingClientRect().width/2 + rowLetters[this.rightLetter].getBoundingClientRect().width/2
         }
     },
     methods: {
         checkLetter(e) {
-            if (e.keyCode === 8)
-                return this.isRed = false
+            switch(e.key) {
+                case 'Backspace':
+                    this.isRed = false 
+                    break
+                case 'CapsLock':
+                    this.isCaps = !this.isCaps;
+                    break
+                case this.arrayOfText[this.rightLetter]:
+                    if (this.rightLetter < 1)
+                        this.timer(60)
+                    
+                    if (this.arrayOfText[this.rightLetter] === ' ')
+                        console.log('end of word')
 
-            if (e.key === this.arrayOfText[this.rightLetter]) {
-                if (this.rightLetter++ === this.arrayOfText.length -1) {
-                    return this.$emit('finished')
-                }
-                return this.isRed = false
+                    this.rightLetter++
+                    this.isRed = false
+                    break
+                default:
+                    this.isRed = true
+                    break
             }
-
-            this.isRed = true
+        },
+        timer(end) {
+            let i = end
+            setInterval(function() {
+                if (i < 0)
+                    return clearInterval()
+                
+                console.log(i--);
+            }.bind(this), 1000)
         }
     },
     mounted() {
         window.addEventListener('keydown', function ($event) {
             this.checkLetter($event)
         }.bind(this))
+
+        rowLetters = document.querySelectorAll('.body-letter')
+        this.offsetX = rowLetters[0].getBoundingClientRect().width/2
     }
 }
 </script>
 
 <style scoped>
+.type-test-container {
+    margin: 0 auto;
+    max-width: 750px;
+    width: 95%;
+}
 .type-test-body {
+    position: relative;
+    padding-left: 50%;
+    overflow: hidden;
+}
+.type-test-body-letter-row {
     display: flex;
-    justify-content: center;
     align-items: stretch;
-    padding-bottom: 20px;
 }
 .body-letter {
     display: inline-block;
-    min-width: 5px;
-    margin: 0 0.025em;
     padding: 0.2em;
     border-radius: 2px;
     font-size: 20px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="home">
-    <Modal title="Test result" :isShow="isShowModal"><p>You pasted test</p></Modal>
+    <!-- <Modal title="Test result" :isShow="isShowModal"><p>You pasted test</p></Modal> -->
     <TypeTest :testText="testText" @finished="typeTestFinished"/>
   </div>
 </template>
 
 <script>
 import TypeTest from '@/components/TypeTest.vue'
-import Modal from '@/components/Modal.vue'
+// import Modal from '@/components/Modal.vue'
 
 export default {
   name: 'Home',
   components: {
     TypeTest,
-    Modal
+    // Modal
   },
   data: () => {
     return {
-      testText: 'Hello world',
+      testText: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum quia, ratione laborum voluptates quibusdam est.',
       isShowModal: false
     }
   },
